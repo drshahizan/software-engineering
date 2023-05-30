e9bedac3ee37ee68e26e9f51197f4ab9e5b5cffa
Navachander Navasantar <navachander@graduate.utm.my>
2023-05-30 15:49:36 +0800

    Add files via upload

---
+++ lab/php/Navachander1/lab3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?>
+</body>
+</html>
