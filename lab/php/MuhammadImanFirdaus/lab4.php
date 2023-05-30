32d30f3aebcf1ce9edca97c713a33e65e17a628c
Muhammad Iman Firdaus <128123046+MuhammadImanFirdaus@users.noreply.github.com>
2023-05-30 15:53:55 +0800

    Create lab4.php

---
+++ lab/php/MuhammadImanFirdaus/lab4.php
@@ -0,0 +1,17 @@
+
+Result Size: 753 x 637
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = "100";
+
+var_dump($x != $y); // returns false because values are equal
+?>  
+
+</body>
+</html>
+
+bool(false)
