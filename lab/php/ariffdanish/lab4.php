79adaea5cabbf45b7670026a1253ade38167824b
Ariff Danish <128231261+ariffdanish@users.noreply.github.com>
2023-05-30 15:51:53 +0800

    Create lab4.php

---
+++ lab/php/ariffdanish/lab4.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = array("a" => "red", "b" => "green");  
+$y = array("c" => "blue", "d" => "yellow");  
+
+print_r($x + $y); // union of $x and $y
+?>  
+
+</body>
+</html>
