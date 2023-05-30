34692117b445024cce00ef25398f3d118e942d6a
VINESH A/L VIJAYAKUMAR <106536953+vinesh06@users.noreply.github.com>
2023-05-30 15:56:46 +0800

    Create lab6.php

---
+++ lab/php/vinesh06/lab6.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello World";?>
+    <?php echo "Hello World";?>
+
+    <?php
+    $x = 5;
+    $y = 4;
+    echo $x + $y;
+    ?>
+
+    <?php
+    echo "<h2>PHP is Fun!</h2>";
+    echo "Hello world!<br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+    ?>
+    
+    <?php
+    $x = 10;  
+    echo --$x;
+    ?> 
+    
+    <?php
+    $favcolor = "red";
+
+    switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red!";
+        break;
+    case "blue":
+        echo "Your favorite color is blue!";
+        break;
+    case "green":
+        echo "Your favorite color is green!";
+        break;
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+    }
+    ?>
+    <?php
+        // define variables and set to empty values
+        $name = $email = $gender = $comment = $website = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $website = test_input($_POST["website"]);
+        $comment = test_input($_POST["comment"]);
+        $gender = test_input($_POST["gender"]);
+        }
+
+        function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }
+        ?>
+
+</body>
+</html>
