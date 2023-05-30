30d4cea636031c91c62279936243ca7a6a8d561f
Wan Nur Sofea <wannursofea@graduate.utm.my>
2023-05-30 15:54:15 +0800

    update lab

---
+++ lab/php/wannursofea/lab5.php
@@ -0,0 +1,25 @@
+
+Result Size: 625 x 414
+<!DOCTYPE html>
+<html>
+<body>
+​<?php
+$favcolor = "red";
+​switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+?>
+ 
+</body>
+</html>
+​Your favorite color is red!
