abc29045affc6c8986a92e13478f29e02e08646c
Muhammad Iman Firdaus <128123046+MuhammadImanFirdaus@users.noreply.github.com>
2023-05-30 16:22:49 +0800

    Create db3.php

---
+++ lab/php/MuhammadImanFirdaus/db3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt create database query execution
+$sql = "CREATE DATABASE demo1";
+if(mysqli_query($link, $sql)){
+    echo "Database created successfully";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+    
+</body>
+</html>
