b075f2fdd93a7323894099471e05a8ea581e3e14
Arini Fatihah <128214992+arinifthh@users.noreply.github.com>
2023-05-30 15:53:32 +0800

    Add files via upload

---
+++ lab/php/arinifthh/lab3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
