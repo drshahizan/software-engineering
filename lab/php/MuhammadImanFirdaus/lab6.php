9719e996c3c3856337a252d37d6204544ee9d13d
Muhammad Iman Firdaus <128123046+MuhammadImanFirdaus@users.noreply.github.com>
2023-05-30 15:56:09 +0800

    Create lab6.php

---
+++ lab/php/MuhammadImanFirdaus/lab6.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
