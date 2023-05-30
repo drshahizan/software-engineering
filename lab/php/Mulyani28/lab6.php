6e4a825c1e68219044e7d0ab2644362a33fc078a
Mulyani Saripuddin <128196055+Mulyani28@users.noreply.github.com>
2023-05-30 15:56:57 +0800

    Add files via upload

---
+++ lab/php/Mulyani28/lab6.php
@@ -0,0 +1,19 @@
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
