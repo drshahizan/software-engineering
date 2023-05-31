276f4ae28732b6f85b838299cec512616fdc6515
Mulyani Saripuddin <128196055+Mulyani28@users.noreply.github.com>
2023-05-31 21:20:31 +0800

    Add files via upload

---
+++ lab/php/Mulyani28/db14.php
@@ -0,0 +1,21 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo1");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt delete query execution
+$sql = "DELETE FROM persons WHERE first_name='padayachy'";
+if(mysqli_query($link, $sql)){
+    echo "Records were deleted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
