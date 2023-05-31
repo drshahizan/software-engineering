15a265ea069b58a803308c4527027bd17260097f
Daniel Hakim <129138344+akim730@users.noreply.github.com>
2023-05-31 14:36:37 +0800

    Add files via upload

---
+++ lab/php/akim730/db12.php
@@ -0,0 +1,21 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo1");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt update query execution
+$sql = "UPDATE persons SET email='change@mail.com' WHERE id=1";
+if(mysqli_query($link, $sql)){
+    echo "Records were updated successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
